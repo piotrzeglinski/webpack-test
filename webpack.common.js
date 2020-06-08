614cd0f327b7c968be2ae17426894450ae4bb85b
Piotr Żegliński <piotr.zeglinski@gmail.com>
2020-06-08 14:24:41 +0100

    new file: zagiel

--- webpack.common.js
+++ webpack.common.js
@@ -63,5 +63,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+        template: "./src/pages/zagiel.html",
+        inject: true,
+        chunks: ["index", "another"],
+        filename: "zagiel.html",
+    }),
   ],
 };
